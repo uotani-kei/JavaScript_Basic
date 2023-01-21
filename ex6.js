0044448f48e4d18a83e7e71c1ca6d1a2258dc4f9
自分のアカウント名 <>
2023-01-21 17:44:12 +0900

    ex

---
+++ ex6.js
@@ -0,0 +1,38 @@
+let a = "あいうえお"
+a{1}
+a [1]
+a [-1]
+[1, 2, 3], ["abc", "cde", "efg"]
+numbers = [1, 2, 3];
+numbers[1]; 
+numbers[2] = 6
+numbers[5] = 9 
+numbers.push(8)
+numbers.pop()
+numbers = [1, 2, 3];
+chars = ["abc", "cde", "efg"];
+new_array = [...numbers, ...chars];
+num = [1,2,3,4]
+ber = ['a','b','c']
+abc = [true,false]
+num.pop()
+num
+ber.pop()
+ber
+abc.pop()
+abc
+aaa = [...num, ...ber, ...abc]
+{"id": 0, "name": "My Name", "own_car": true}
+info = {"id": 0, "name": "My Name", "own_car": true};
+info.id;
+info["name"];
+info.id = 5;
+info["own_car"] = false;
+info.age = 18 
+"age" in info;
+"parent" in info;
+delete info.age;
+"age" in info;
+bbb = {"name":"user"}
+ccc = {"pass":"0713"}
+ddd = {...bbb, ...ccc}
---
+++ ex9.js
@@ -0,0 +1,84 @@
+let a = "あいうえお"
+a{1}
+a [1]
+a [-1]
+[1, 2, 3], ["abc", "cde", "efg"]
+numbers = [1, 2, 3];
+numbers[1]; 
+numbers[2] = 6
+numbers[5] = 9 
+numbers.push(8)
+numbers.pop()
+numbers = [1, 2, 3];
+chars = ["abc", "cde", "efg"];
+new_array = [...numbers, ...chars];
+num = [1,2,3,4]
+ber = ['a','b','c']
+abc = [true,false]
+num.pop()
+num
+ber.pop()
+ber
+abc.pop()
+abc
+aaa = [...num, ...ber, ...abc]
+{"id": 0, "name": "My Name", "own_car": true}
+info = {"id": 0, "name": "My Name", "own_car": true};
+info.id;
+info["name"];
+info.id = 5;
+info["own_car"] = false;
+info.age = 18 
+"age" in info;
+"parent" in info;
+delete info.age;
+"age" in info;
+bbb = {"name":"user"}
+ccc = {"pass":"0713"}
+ddd = {...bbb, ...ccc}
+const PI = 3.14
+pi = 3.14
+a = [1,2,3,4]
+for (let b for a){}
+a = [1,2,3,4]
+for (let b for a){
+if(a === 3){
+console.log("aは3です。")
+}
+}
+
+a = [1,2,3,4]
+for (let b of a){
+if(a === 3){
+console.log("aは3です。")
+}
+}
+
+a = [1,2,3,4]
+for (let b of a){
+if(b === 3){
+console.log("bは3です。")
+}
+}
+
+function ber(h,i){
+return
+h + i;
+}
+
+ber(1,2)
+function ber(h,i){
+return h + i;
+}
+
+ber(1,2)
+const now = function(a,b){
+return a+b;
+}
+
+now(100,100)
+const moon = (a,b) =>{
+return a+b;
+}
+
+moon(15,35)
